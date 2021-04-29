5a28bdb15a2b46a377a0f09c7c1bdc39fd2c074b
lijianru <2219269374@qq.com>
2021-04-29 19:21:30 +0800

    update storybook

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { Button, ButtonProps } from '../components/Button';
+import { Button, ButtonProps } from '../src/components/Button';
 
 export default {
   title: 'Example/Button',
