42bcc07f8c6b87df1fd3fb54e4ca8a2f238d8977
lijianru <2219269374@qq.com>
2021-07-22 11:32:43 +0800

    update about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -7,33 +7,40 @@ import useAsyncFunction from '../../hooks/useAsyncFunction';
 export function About() {
   const [value, setValue] = useState<number>();
 
-  // const [state, callback] = useAsyncFunction(async (val: number) => {
-  //   await axios.get(`api/todos/${val}`);
+  const [state, callback] = useAsyncFunction(async (val: number) => {
+    await axios.get(`api/todos/${val}`);
 
-  //   console.log('fetch', val);
-  // }, []);
+    console.log('fetch', val);
+  }, []);
 
   // useEffect(() => {
   //   console.log('useEffect', value);
   //   value && callback(value);
   // }, [value]);
 
-  const [state, callback] = useAsyncFunction(async () => {
-    await axios.get(`api/todos/${value}`);
+  // const [state, callback] = useAsyncFunction(async () => {
+  //   await axios.get(`api/todos/${value}`);
 
-    console.log('fetch', value);
-  }, [value]);
+  //   console.log('fetch', value);
+  // }, [value]);
 
-  useEffect(() => {
-    console.log('useEffect', value);
-    value && callback();
-  }, [callback]);
+  // useEffect(() => {
+  //   console.log('useEffect', value);
+  //   value && callback();
+  // }, [callback]);
 
   console.log('render', value);
 
   return (
     <div style={{ margin: '100px' }}>
-      <InputNumber value={value} onChange={val => setValue(val)} />
+      {/* <InputNumber value={value} onChange={val => setValue(val)} /> */}
+      <InputNumber
+        value={value}
+        onChange={val => {
+          setValue(val);
+          callback(val);
+        }}
+      />
       <a>{Math.random()}</a>
       <p>{`${state.loading}`}</p>
     </div>
