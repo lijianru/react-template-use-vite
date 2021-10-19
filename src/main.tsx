8c76352019182b2dc7d549ad9469952bdfa232d7
lijianru <2219269374@qq.com>
2021-10-19 14:56:13 +0800

    fix tailwindcss本地和build样式引入顺序不一致

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import 'tailwindcss/tailwind.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -7,6 +5,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { store, persistor } from './state-container/store';
+
+// 保证样式在所有组件引用的最下方，否则将导致本地和build样式引入顺序不一致
+import 'tailwindcss/tailwind.css';
 
 ReactDOM.render(
   <React.StrictMode>
