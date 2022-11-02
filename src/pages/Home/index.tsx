5eb8d389e0f2023273542ae12a594faf2da2102c
lijianru <2219269374@qq.com>
2022-11-02 14:22:08 +0800

    test code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -44,6 +44,13 @@ export default function Home() {
           <p className="text-gray-500">You have a new message!</p>
         </div>
       </div>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 mt-8">
+        <div className="flex-shrink-0"></div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
       <img src={banana} />
     </div>
   );
